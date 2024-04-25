154ccee3b3ebf2c7f755d72322a02e6b99886672
REINERisavailable <139576975+REINERisavailable@users.noreply.github.com>
2024-04-25 17:07:53 +0100

    Add files via upload

---
+++ mood.py
@@ -0,0 +1,9 @@
+# Python
+import streamlit as st
+
+def main():
+    st.title('Hello, Streamlit!')
+    st.write('This is a simple Streamlit app for deployment.')
+
+if __name__ == "__main__":
+    main()

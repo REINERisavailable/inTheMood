004068f7d724f768f5fb15ee05b59f5d65b3eb66
REINERisavailable <139576975+REINERisavailable@users.noreply.github.com>
2024-04-25 17:31:27 +0100

    S.py

--- mood.py
+++ mood.py
@@ -12,13 +12,6 @@ def main():
         if st.button("Recommend me"):
             st.write(f"Here are some {activity.lower()} recommendations for when you're feeling {mood.lower()}:")
 
-            # Here you can add code to display recommendations based on the selected activity and mood
-            # For example:
-            if activity == "Watching a movie" and mood == "Happy":
-                st.write("Movie 1, Movie 2, Movie 3")
-            elif activity == "Reading a book" and mood == "Sad":
-                st.write("Book 1, Book 2, Book 3")
-            # Add more conditions to handle other combinations of activity and mood
-
+            
 if __name__ == "__main__":
     main()
